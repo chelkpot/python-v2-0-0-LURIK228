5ed5319f9aa86dda6d455783d0ea4c33c96ed0b7
Admin <lurik228@yandex.ru>
2025-10-01 12:01:59 +0500

    уберите 2 за html, у меня все правильно, 2 задание исправил

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,9 +2,7 @@
 
 def solve():
 # Ниже пишите решение задачи
-    Z=int(input("Введите количество карандашей: "))
-    O=int(input("Введите количество ручек: "))
-    V=int(input("Введите количество фломастеров: "))
+    Z,O,V=map(int,input().split())
     cena_karandash=3
     cena_ruchki=cena_karandash+2
     cena_flomastera=cena_ruchki+7
