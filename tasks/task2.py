3cd65fc60ec1f9e27c30da18bf0d7b11dc83a1c2
Admin <lurik228@yandex.ru>
2025-10-01 11:50:22 +0500

    уберите 2 за html, у меня все правильно

--- tasks/task2.py
+++ tasks/task2.py
@@ -2,10 +2,14 @@
 
 def solve():
 # Ниже пишите решение задачи
-
-   
-
-   
+    Z=int(input("Введите количество карандашей: "))
+    O=int(input("Введите количество ручек: "))
+    V=int(input("Введите количество фломастеров: "))
+    cena_karandash=3
+    cena_ruchki=cena_karandash+2
+    cena_flomastera=cena_ruchki+7
+    vsego=(Z*cena_karandash)+(O*cena_ruchki)+(V*cena_flomastera)
+    print("Общая стоимость покупки:",vsego)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
--- tasks/task3.py
+++ tasks/task3.py
@@ -2,9 +2,8 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-
-
+    q,e = map(int, input().split())
+    print(q-1,e-1)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
