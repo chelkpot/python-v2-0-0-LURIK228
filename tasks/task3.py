328ffac1b728878a3d05bc2c618c08fb6d310d93
Admin <lurik228@yandex.ru>
2025-10-01 12:06:49 +0500

    уберите 2 за html, у меня все правильно, 2 задание исправил(Снова)(снова)

--- tasks/task3.py
+++ tasks/task3.py
@@ -3,7 +3,7 @@
 def solve():
 # Ниже пишите решение задачи
     q,e = map(int, input().split())
-    print(q-1,e-1)
+    print(e-1,q-1)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
